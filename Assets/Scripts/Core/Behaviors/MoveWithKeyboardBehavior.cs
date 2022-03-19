def0f2099fe665d01755dc82e788169b483ddb34
Marvin <marvin@koch-family.ch>
2022-03-19 15:10:28 +0100

    Added Cellulo Movement

--- Assets/Scripts/Core/Behaviors/MoveWithKeyboardBehavior.cs
+++ Assets/Scripts/Core/Behaviors/MoveWithKeyboardBehavior.cs
@@ -14,6 +14,24 @@ public class MoveWithKeyboardBehavior : AgentBehaviour
     public override Steering GetSteering()
     {
         Steering steering = new Steering();
+        float horizontal = 0;
+        float vertical = 0;
+        if(inputKeyboard == InputKeyboard.arrows)
+        {
+            horizontal = Input.GetAxis("Horizontal");
+            vertical = Input.GetAxis("Vertical");
+
+
+        }
+        else
+        {
+            horizontal = Input.GetAxis("HorizontalWASD");
+            vertical = Input.GetAxis("VerticalWASD");
+
+        }
+        steering.linear = new Vector3(horizontal, 0,  vertical) * agent.maxAccel;
+        steering.linear = this.transform.parent.TransformDirection(Vector3.ClampMagnitude(steering.
+        linear, agent.maxAccel));
         //implement your code here
         return steering;
     }
