e97cd775a2d08388f99ddb34a9534b3478e7a0a3
Svanniah <75037332+Svanniah@users.noreply.github.com>
2022-03-31 21:52:59 +0200

    Button functionality in game-over scene

---
+++ Assets/Scripts/UI/Menu_functionality.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu_functionality : MonoBehaviour
+{
+    public Button button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button menu = button.GetComponent<Button>();
+        menu.onClick.AddListener(TaskOnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void TaskOnClick(){
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/Scripts/UI/Restart_functionality.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class Restart_functionality : MonoBehaviour
+{
+    public Button button;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button restart = button.GetComponent<Button>();
+        restart.onClick.AddListener(TaskOnClick);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void TaskOnClick() {
+        SceneManager.LoadScene(0);
+    }
+}
