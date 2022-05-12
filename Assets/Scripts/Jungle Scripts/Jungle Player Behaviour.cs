70ef7c6543ed53dd070ae1c0cd13dec83afe54bd
Marvin Koch <marvin@tsf-476-wpa-1-165.epfl.ch>
2022-05-12 12:02:46 +0200

    wip

---
+++ Assets/Scripts/Jungle Scripts/Jungle Player Behaviour.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JunglePlayerBehaviour : AgentBehaviour
+{
+    int MONKEY_TOUCH_TIME = 2;
+    int TOUCAN_TOUCH_TIME = 2;
+    int SLOTH_TOUCH_TIME = 2;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public override Steering GetSteering()
+    {
+        Steering steering = new Steering();
+        input = (player == 2) ? PlayerSettings.input1 : PlayerSettings.input2;
+        float horizontal = 0;
+        float vertical = 0;
+        //print(string.Format("Keyboard Player {0} is active : {1}", player, activeKey));
+        //if (input == 0 || input == 1)
+        //{
+            //print(string.Format("Keyboard Player {0} input is  : {1}", player, input));
+        if(!Timer.paused){
+
+            if (input == 1)
+            {
+
+                //print("Getting Arrows");
+                horizontal = Input.GetAxis("Horizontal");
+                vertical = Input.GetAxis("Vertical");
+                steering.linear = new Vector3(horizontal, 0, vertical) * agent.maxAccel;
+                steering.linear = this.transform.parent.TransformDirection(Vector3.ClampMagnitude(steering.
+                linear, agent.maxAccel));
+            }
+            else if(input == 0)
+            {
+                //print("Getting WASD");
+                horizontal = Input.GetAxis("HorizontalWASD");
+                vertical = Input.GetAxis("VerticalWASD");
+                //print("Finished WASD");
+                steering.linear = new Vector3(horizontal, 0, vertical) * agent.maxAccel;
+                steering.linear = this.transform.parent.TransformDirection(Vector3.ClampMagnitude(steering.
+                linear, agent.maxAccel));
+            } 
+            else 
+            {
+                Vector3 position;
+                Vector3 direction;
+                if (Input.GetMouseButton(0))
+                {
+                    position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                    direction = (position - transform.position).normalized;
+                }
+                else
+                {
+                    direction = Vector3.zero;
+                }
+                steering.linear = direction * agent.maxAccel;
+                steering.linear = this.transform.parent.TransformDirection(Vector3.ClampMagnitude(steering.
+                linear, agent.maxAccel));
+            }
+
+        }
+       
+        return steering;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        /**
+        if (collision.gameObject.CompareTag("Player") && this.GetComponent<public_variables>().stealer)
+        {
+            this.GetComponent<public_variables>().score = this.GetComponent<public_variables>().score + 2;
+            collision.gameObject.GetComponent<public_variables>().score = collision.gameObject.GetComponent<public_variables>().score - 2;
+            this.GetComponent<public_variables>().stealer = false;
+            
+        }
+        */
+
+        if(this.gameObject.CompareTag("Monkey")){
+            if(collision.gameObject.CompareTag("Toucan")){
+                this.GetComponent<public_variables>().score += 1;
+            }
+        }else if(this.gameObject.CompareTag("Toucan")){
+            if(collision.gameObject.CompareTag("Sloth")){
+                this.GetComponent<public_variables>().score += 1;
+            }
+        }else{
+            if(collision.gameObject.CompareTag("Monkey")){
+                this.GetComponent<public_variables>().score += 1;
+            }
+        }
+
+    }
+
+    private void OnCollisionExit(CollectionExtensions collision){
+
+        // Compare le temps à l'entrée et la sorte
+
+
+    }
+
+
+
+    public override void OnCelluloLongTouch(int key){
+        long_pressing = true;
+    }
+}
